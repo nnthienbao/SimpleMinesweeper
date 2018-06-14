c9c987bedd54a155ca3c9fc213f955ff021c2e9e
Unknown <nnthienbao040@gmail.com>
2018-06-14 21:33:09 +0700

    Fix loi ve ban co

--- src/main/java/gui/Board.java
+++ src/main/java/gui/Board.java
@@ -50,6 +50,7 @@ public class Board extends JPanel implements MouseMotionListener, MouseListener,
 
     @Override
     protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
         //Draw cell
